dc32ba92719587509ab36d14bb2e22db66adf83a
Carlos Calderón Montes <carloscalderon1994@hotmail.com>
2016-08-01 10:54:50 -0500

    finalizacion

--- app/src/main/java/com/facci/restaurantcc/MainActivityCC.java
+++ app/src/main/java/com/facci/restaurantcc/MainActivityCC.java
@@ -2,6 +2,8 @@ package com.facci.restaurantcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivityCC extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivityCC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_cc);
+    }
+
+    public void click1(View v) {
+        Toast.makeText(MainActivityCC.this,"Bandeja de Mariscos",Toast.LENGTH_SHORT).show();
+    }
+
+    public void click2(View v) {
+        Toast.makeText(MainActivityCC.this,"Caldo de Gallina",Toast.LENGTH_SHORT).show();
+    }
+
+    public void click3(View v) {
+        Toast.makeText(MainActivityCC.this,"Ceviche de Camarón",Toast.LENGTH_SHORT).show();
+    }
+
+    public void click4(View v) {
+        Toast.makeText(MainActivityCC.this,"Corviche",Toast.LENGTH_SHORT).show();
+    }
+
+    public void click5(View v) {
+        Toast.makeText(MainActivityCC.this,"Pescado Frito",Toast.LENGTH_SHORT).show();
+    }
+
+    public void click6(View v) {
+        Toast.makeText(MainActivityCC.this,"Salprieta",Toast.LENGTH_SHORT).show();
+    }
+
+    public void click7(View v) {
+        Toast.makeText(MainActivityCC.this,"Arroz de Mariscos",Toast.LENGTH_SHORT).show();
     }
 }
