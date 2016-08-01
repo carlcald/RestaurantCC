ca465d850a44f4cbed9a59f0905088b7d8c1be42
Carlos Calderón Montes <carloscalderon1994@hotmail.com>
2016-07-31 21:23:18 -0500

    Initial commit

---
+++ app/src/main/java/com/facci/restaurantcc/MainActivityCC.java
@@ -0,0 +1,13 @@
+package com.facci.restaurantcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityCC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activity_cc);
+    }
+}
